b5e80b368093f4564390e68c68bebd7a4338a9cd
mallum <mallum@b067294f-1dea-0310-9683-c47a78595994>
2005-09-26 16:27:28 +0000

    improve docbuilding script
    git-svn-id: https://svn.o-hand.com/repos/matchbox/trunk/matchbox-documentation@1215 b067294f-1dea-0310-9683-c47a78595994

--- make-docs.sh
+++ make-docs.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 rm -fr /tmp/mb-docs
+rm -fr mb-docs.tar.gz
 
 # manual
 echo "Generating HTML Manual"
-mkdir -p /tmp/mb-docs/manual
+mkdir -p /tmp/mb-docs/manual/images
 xsltproc -o /tmp/mb-docs/manual/ --param use.id.as.filename 1 common/mb-dbk.xsl  manual/matchbox-manual.xml
-cp -r common/images common/mb.css /tmp/mb-docs/manual/
+cp -r common/mb.css /tmp/mb-docs/manual/
+cp -r common/images/*.png /tmp/mb-docs/manual/images/
 
 echo "Generating manual tarball"
-tar cvzf /tmp/mb-docs/matchbox-manual.tar.gz /tmp/mb-docs/manual
+tar cvzf --exclude=.svn /tmp/mb-docs/matchbox-manual.tar.gz /tmp/mb-docs/manual
 
 echo "Generating manual PDF"
 echo "Not working as yet.."
 #xsltproc --output /tmp/mb-docs/manual/matchbox-manual.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/fo.xsl manual/matchbox-manual.xml
 
 echo "Generating Testing doc"
-mkdir -p /tmp/mb-docs/developers/testing
+mkdir -p /tmp/mb-docs/developers/testing/images
 xsltproc -o /tmp/mb-docs/developers/testing/ --param use.id.as.filename 1 common/mb-dbk.xsl testing/testing.docbook
-cp -r common/images common/mb.css /tmp/mb-docs/developers/testing/
+cp -r common/mb.css /tmp/mb-docs/developers/testing/
+cp -r common/images/*.png /tmp/mb-docs/developers/testing/images/
 
 echo "Generating Themeing doc"
 
-mkdir -p /tmp/mb-docs/themes
+mkdir -p /tmp/mb-docs/themes/images
 xsltproc -o /tmp/mb-docs/themes/ --param use.id.as.filename 1 common/mb-dbk.xsl theme-howto/theme.howto.docbook
-cp -r theme-howto/*.png common/images common/mb.css /tmp/mb-docs/themes
+cp -r theme-howto/*.png common/mb.css /tmp/mb-docs/themes
+cp -r common/images/*.png /tmp/mb-docs/themes/images/
+
+echo "Copying various text files and stuff into place"
+cp developers/internals.txt developers/matchbox-key.txt developers/matchbox.pdf developers/internals.dia /tmp/mb-docs/developers/
+
+echo "Tarballing"
+tar cvzf mb-docs.tar.gz /tmp/mb-docs/*
+echo
+echo "All done. Untar mb-docs.tar.gz in web documentation dir. Remember api docs"
+echo
